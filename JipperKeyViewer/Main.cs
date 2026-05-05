84bbb13578e942752923928e69407051909e6966
HitMargin <hitmargin@qq.com>
2026-05-05 23:46:21 +0800

    精简

--- JipperKeyViewer/Main.cs
+++ JipperKeyViewer/Main.cs
@@ -1,11 +1,5 @@
-﻿using HarmonyLib;
 using JipperKeyViewer.KeyViewer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using UnityModManagerNet;
 using UnityEngine;
 
@@ -13,26 +7,18 @@ namespace JipperKeyViewer
 {
     public class Main
     {
-#pragma warning disable CS8632 // 只能在 "#nullable" 注释上下文内的代码中使用可为 null 的引用类型的注释。
-        public static UnityModManager.ModEntry? Mod { get; private set; }
-#pragma warning restore CS8632 // 只能在 "#nullable" 注释上下文内的代码中使用可为 null 的引用类型的注释。
-#pragma warning disable CS8632 // 只能在 "#nullable" 注释上下文内的代码中使用可为 null 的引用类型的注释。
-        public static Harmony? Harmony { get; private set; }
-#pragma warning restore CS8632 // 只能在 "#nullable" 注释上下文内的代码中使用可为 null 的引用类型的注释。
+        public static UnityModManager.ModEntry Mod { get; private set; }
 
         static GameObject KeyViewerGO;
+
         public static bool Load(UnityModManager.ModEntry modEntry)
         {
             Mod = modEntry;
 
-            // Setup callbacks / 设置回调
             modEntry.OnToggle = OnToggle;
             modEntry.OnGUI = (entry) => KeyViewer.KeyViewer.instance?.DrawSettingsWindow();
 
-            // Create Harmony instance / 创建 Harmony 实例
-            Harmony = new Harmony(modEntry.Info.Id);
-
-            modEntry.Logger.Log("Mod loaded / Mod 已加载");
+            modEntry.Logger.Log("Mod loaded");
             return true;
         }
 
@@ -40,8 +26,6 @@ namespace JipperKeyViewer
         {
             if (value)
             {
-                Harmony?.PatchAll(Assembly.GetExecutingAssembly());
-
                 if (KeyViewerGO == null)
                 {
                     KeyViewerGO = new GameObject("JipperKeyViewer");
@@ -51,8 +35,6 @@ namespace JipperKeyViewer
             }
             else
             {
-                Harmony?.UnpatchAll(modEntry.Info.Id);
-
                 if (KeyViewerGO != null)
                 {
                     GameObject.Destroy(KeyViewerGO);
